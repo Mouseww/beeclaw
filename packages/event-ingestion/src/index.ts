503703ed0f9696300ea6a26ea0bccb747569793e
root <root@ser428103129482.local>
2026-03-13 09:03:37 +0800

    chore: 更新架构文档和 event-ingestion index 导出

--- packages/event-ingestion/src/index.ts
+++ packages/event-ingestion/src/index.ts
@@ -5,6 +5,8 @@
 export { EventIngestion } from './EventIngestion.js';
 export { parseFeed } from './FeedParser.js';
 export { ImportanceEvaluator } from './ImportanceEvaluator.js';
+export { FinanceDataSource, POPULAR_STOCKS, POPULAR_CRYPTO, POPULAR_INDICES } from './FinanceDataSource.js';
+export { MarketSentiment } from './MarketSentiment.js';
 export type {
   FeedSource,
   FeedSourceType,
@@ -12,4 +14,10 @@ export type {
   ParsedFeed,
   ImportanceAssessment,
   EventIngestionConfig,
+  AssetType,
+  FinanceSymbol,
+  FinanceSourceConfig,
+  QuoteData,
+  MarketSentimentType,
+  MarketSentimentResult,
 } from './types.js';
